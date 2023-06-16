de70364e83930d8fd0aca3dc89d6159c9a65283b
David Gherlovanu <dgherlovanu098890@gmail.com>
2023-06-16 23:06:34 +0300

    Adding framer motion to the project

--- components/index/Reviews.tsx
+++ components/index/Reviews.tsx
@@ -1,4 +1,13 @@
 export default function Reviews() {
+    const reviews = [
+        {
+            id: 1,
+            text: "David has shown a very professional and caring attitude. He delivered the website with all the requested features and always communicated regarding things that weren't clear. Very happy with the result overall! Would hire him again if I had the chance.",
+            stars: 5,
+            name: "Matvey Dergunov",
+        },
+    ];
+
     return (
         <section aria-label="Reviews" className="container mt-[128px]">
             <div className="font-medium text-left font-space">
@@ -7,7 +16,7 @@ export default function Reviews() {
             </div>
             <div className="mt-[64px] flex">
                 {/* First review */}
-                <div className="review">
+                {/* <div className="review">
                     <p className="max-w-xs italic">
                         David has shown a very professional and caring attitude.
                         He delivered the website with all the requested features
@@ -23,7 +32,21 @@ export default function Reviews() {
                         <img src="./star.svg" alt="" />
                     </div>
                     <p className="text-[20px]">Matvey Dergunov</p>
-                </div>
+                </div> */}
+
+                {reviews.map((review) => (
+                    <div key={review.id} className="review">
+                        <p className="max-w-xs italic">{review.text}</p>
+                        <div className="flex gap-2">
+                            {Array.from({ length: review.stars }).map(
+                                (_, index) => (
+                                    <img key={index} src="./star.svg" alt="" />
+                                )
+                            )}
+                        </div>
+                        <p className="text-[20px]">{review.name}</p>)
+                    </div>
+                ))}
             </div>
         </section>
     );
